df0e9852e408938613080f8d9af6319605931981
scoophamp <hampus_cousin@hotmailcom>
2017-05-22 13:15:19 +0200

    First test, complete

---
+++ TDD-Exercises/ValidationEngine/Validator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ValidationEngine
+{
+    public class Validator
+    {
+        
+       private string validEmail = @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+
+        public bool ValidateEmailAdress(string v)
+        {
+            var resultMatch = Regex.IsMatch(v, validEmail);
+            return resultMatch;
+            
+        }
+    }
+}
--- TDD-Exercises/ValidationEngineTests/ValidationTests.cs
+++ TDD-Exercises/ValidationEngineTests/ValidationTests.cs
@@ -4,12 +4,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ValidationEngine;
 
 namespace ValidationEngineTests
 {
    public class ValidationTests
     {
-        
+        [Test]
+        public void TrueForValidAddress()
+        {
+            var sut = new Validator();
+
+            sut.ValidateEmailAdress("hampus_cousin@hotmail.com");
+
+            Assert.IsTrue(true);
+        }
 
 
     }
